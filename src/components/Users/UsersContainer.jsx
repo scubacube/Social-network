83c9e791316077d9241583b144a9b8e4160015b3
Alex <alexey.chekhotskiy@gmail.com>
2020-11-07 18:55:32 +0100

    added pagination on users page, refarctored container user component using connect()

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,7 +1,58 @@
 import React from "react";
 import {connect} from "react-redux";
-import {followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unFollowAC} from "../../redux/UsersReducer";
+import {
+    followAC,
+    isFetchingAC,
+    setCurrentPageAC,
+    setTotalUsersCountAC,
+    setUsersAC,
+    unFollowAC
+} from "../../redux/UsersReducer";
 import Users from "./Users";
+import * as axios from "axios";
+
+
+
+class UsersAPI extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    componentDidMount() {
+        this.props.isFetchingFunc(true);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}
+        &count=${this.props.pageSize}`).then(resp =>
+        {
+            this.props.isFetchingFunc(false);
+            this.props.setUser(resp.data.items);
+            this.props.setTotalUsersCount(resp.data.totalCount)
+        });
+    }
+    onChanged = (cp) => {
+        this.props.isFetchingFunc(true);
+        this.props.setCurrentPage(cp);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${cp}
+        &count=${this.props.pageSize}`).then(resp =>
+        {
+            this.props.isFetchingFunc(false);
+            this.props.setUser(resp.data.items)
+        });
+    }
+    followCB = (id) => {
+        this.props.follow(id);
+    }
+    unfollowCB = (id) => {
+        this.props.unfollow(id);
+    }
+    render() {
+        return <Users onChanged={this.onChanged}
+                      users={this.props.users}
+                      followCB={this.followCB}
+                      unfollowCB={this.unfollowCB}
+                      totalCount={this.props.totalCount}
+                      pageSize={this.props.pageSize}
+                      isFetching={this.props.isFetching}/>
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -28,8 +79,11 @@ let mapDispatchToProps = (dispatch) => {
         },
         setTotalUsersCount: (tCount) => {
             dispatch(setTotalUsersCountAC(tCount));
+        },
+        isFetchingFunc: (isF) => {
+            dispatch(isFetchingAC(isF));
         }
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersAPI);
--- src/redux/UsersReducer.js
+++ src/redux/UsersReducer.js
@@ -3,18 +3,27 @@ const FOLLOW = "FOLLOW";
 const UNFOLLOW = "UNFOLLOW";
 const SET_CURRENT_PAGE = "SET-CURRENT-PAGE";
 const SET_TOTAL_USERS_COUNT = "SET-TOTAL-USERS-COUNT";
+const IS_FETCHING = "IS_FETCHING"
 
 let initState = {
     users: [],
     pageSize: 5,
     totalCount: 0,
-    currentPage: 1
+    currentPage: 1,
+    isFetching: true
 }
 
 export let setUsersAC = (users) => {
     return ({
         type: SET_USERS,
         users
+    })
+}
+
+export let isFetchingAC = (isFetching) => {
+    return ({
+        type: IS_FETCHING,
+        isFetching
     })
 }
 
@@ -52,7 +61,7 @@ export let userReducer = (state = initState, action) => {
         case FOLLOW:
             return {
                 ...state,
-                users: state.map(e => {
+                users: state.users.map(e => {
                     if(e.id === action.id) {
                         return{...e, followed: true}
                     }
@@ -62,35 +71,40 @@ export let userReducer = (state = initState, action) => {
         case UNFOLLOW:
             return {
                 ...state,
-                users: state.map(e => {
+                users: state.users.map(e => {
+
                     if(e.id === action.id) {
+
                         return{...e, followed: false}
                     }
                     return e;
+
                 })
             }
-
         case SET_USERS: {
             return {
                 ...state,
                 users: [...action.users]
             }
         }
-
         case SET_CURRENT_PAGE: {
             return {
                 ...state,
                 currentPage: action.currentPage
             }
         }
-
         case SET_TOTAL_USERS_COUNT: {
             return {
                 ...state,
                 totalCount: action.totalCount
             }
         }
-
+        case IS_FETCHING: {
+            return  {
+                ...state,
+                isFetching: action.isFetching
+            }
+        }
         default: return state;
     }
 
